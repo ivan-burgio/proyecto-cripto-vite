3197a6d36d54c9eca99c6cbea4f23a6a618eaedb
ivan-burgio <ivan.24.burgio@gmail.com>
2024-07-23 15:57:25 -0300

    un poco de formateo

--- src/schema/crypto-schema.ts
+++ src/schema/crypto-schema.ts
@@ -1,29 +1,31 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const CurrencySchema = z.object({
     code: z.string(),
-    name: z.string()
-})
+    name: z.string(),
+});
 
 export const CryptoCurrencyResponseSchema = z.object({
-    CoinInfo : z.object({
+    CoinInfo: z.object({
         FullName: z.string(),
-        Name: z.string()
-    })
-})
+        Name: z.string(),
+    }),
+});
 
-export const CryptoCurrenciesResponseSchema = z.array(CryptoCurrencyResponseSchema)
+export const CryptoCurrenciesResponseSchema = z.array(
+    CryptoCurrencyResponseSchema
+);
 
 export const PairSchema = z.object({
     currency: z.string(),
-    criptocurrency: z.string()
-})
+    criptocurrency: z.string(),
+});
 
 export const CryptoPriceSchema = z.object({
-    IMAGEURL : z.string(),
+    IMAGEURL: z.string(),
     PRICE: z.string(),
     HIGHDAY: z.string(),
     LOWDAY: z.string(),
     CHANGEPCT24HOUR: z.string(),
-    LASTUPDATE: z.string()
-})
+    LASTUPDATE: z.string(),
+});
